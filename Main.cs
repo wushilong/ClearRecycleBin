39bc87513293f405e1bcc579f9ed5c4217eb413d
codemonk <codemonk@live.cn>
2023-10-07 22:04:48 +0800

    first

---
+++ Main.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System.Runtime.InteropServices;
+using ManagedCommon;
+using Wox.Plugin;
+
+namespace ClearRecycleBin
+{
+    enum RecycleFlags : uint
+    {
+        SHRB_NOCONFIRMATION = 0x00000001, // Don't ask confirmation
+        SHRB_NOPROGRESSUI = 0x00000002, // Don't show any windows dialog
+        SHRB_NOSOUND = 0x00000004 // Don't make sound, ninja mode enabled :v
+    }
+
+    public class Main : IPlugin
+    {
+        public string Name => nameof(ClearRecycleBin);
+        public string Description => "Clear RecycleBin";
+        private string? IconPath { get; set; }
+
+        public void Init(PluginInitContext context)
+        {
+            context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(context.API.GetCurrentTheme());
+        }
+
+        private void OnThemeChanged(Theme currentTheme, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+
+        private void UpdateIconPath(Theme theme)
+        {
+            var isLight = theme == Theme.Light || theme == Theme.HighContrastWhite;
+            IconPath = isLight ? "Images/ClearRecycleBin.light.png" : "Images/ClearRecycleBin.dark.png";
+        }
+
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+
+        public List<Result> Query(Query query)
+        {
+            string message;
+            try
+            {
+                var code = SHEmptyRecycleBin(IntPtr.Zero, "", RecycleFlags.SHRB_NOCONFIRMATION);
+                message = code == 0 ? $"The recycle bin has been succesfully recycled" : $"The recycle bin couldn't be recycled, code: {code}";
+            }
+            catch (Exception ex)
+            {
+                message = $"The recycle bin couldn't be recycled, {ex.Message}";
+            }
+
+            return new List<Result> {
+                new() {
+                    Title = message,
+                    IcoPath = IconPath,
+                    Action = e =>true
+                },
+            };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ClearRecycleBin
+# Clear RecycleBin for PowerToys Run
+
+Clear RecycleBin for PowerToys Run. Simply invoke the command `erb!` and clear recycle-bin.
+
+
+## Installation
+
+**Minimum required version of PowerToys is 0.74.1 - make sure you've got this version installed.**
+
+Exit PowerToys, and save [ClearRecycleBin.zip](https://github.com/wushilong/ClearRecycleBin/releases/download/0.1.0/ClearRecycleBin.zip) to `%LOCALAPPDATA%\Microsoft\PowerToys\PowerToys Run\Plugins` (or wherever your PowerToys is installed), and start PowerToys.
+
+
+
+## Usage
+
+Open up PowerToys Run, and type the command `erb!`.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
